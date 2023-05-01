4b6aeb27bad8c12d71f13e797460566ed26b97d3
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-05-02 01:34:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import { useState } from 'react'
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
 function App() {
-  const name = "anirudh"
-  const x = true
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id:1,
+            text:'Doctors Appointment',
+            day: 'May 5th',
+            reminder: true
+        },
+        {
+            id:2,
+            text:'Meeting',
+            day: 'May 6th',
+            reminder: true
+        },
+        {
+            id:3,
+            text:'Shopping',
+            day: 'May 7th',
+            reminder: false
+        }
+    ]
+)
+
+//DELETE TASK
+const deleteTask = (id) => {
+  console.log('delete', id)
+}
   return (
     <div className="container">
-      <Header/>
-
+      <Header />
+      <Tasks  tasks = {tasks} onDelete={deleteTask} />
     </div>
   );
 }
