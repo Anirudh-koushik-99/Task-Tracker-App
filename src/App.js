81ddc83f5f5093756f84b9e5e2fb7457ab588960
B A Anirudh Koushik <anirudh.baak@gmail.com>
2023-05-02 02:42:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -28,12 +28,21 @@ function App() {
 
 //DELETE TASK
 const deleteTask = (id) => {
-  console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
 }
+
+//TOGGLE REMINDER
+const toggleReminder = (id) =>{
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
+
   return (
     <div className="container">
       <Header />
-      <Tasks  tasks = {tasks} onDelete={deleteTask} />
+      { tasks.length > 0 ? 
+        <Tasks  tasks = {tasks} onDelete={deleteTask} onToggle = {toggleReminder}/> : 'No Tasks to show'
+      }
     </div>
   );
 }
